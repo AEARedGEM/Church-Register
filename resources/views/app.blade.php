b90cf513655c0e74877f0c6672ae4a96bcd34ec3
unknown <crownpaysme19@gmail.com>
2025-12-24 13:15:24 +0100

    OG & Twitter Cards Fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title inertia>{{ config('app.name', 'NYP-IP-Portal') }}</title>
 
         <!-- Fonts -->
@@ -11,18 +10,20 @@
         <link rel="icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <meta name="twitter:title" content="NYP-IP Portal" />
-        <meta name="twitter:description" content="NYP-IP Is A National-Scale, Youth-Driven Industrialization, Entrepreneurship and DeFi Platform for Innovation, Inclusion & Growth. NYP-IP Is Enabling 10 Million Nigerian Youths To Become Builders Of Real Industry. This Initiative Is Powered by LuxuryX Technologies & TradeFi Ltd. In Conjunction With The Nigerian Youth Parliament." />
-        <meta name="twitter:image" content="{{asset('images/logo.svg')}}" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="NYP-IP Portal | Industrialization" />
+        <meta name="twitter:description" content="NYP-IP Is A National-Scale, Youth-Driven Industrialization & Entrepreneurship Platform Enabling Nigerian Youths To Become Builders Of Real Industry. This Initiative Is Powered by LuxuryX Technologies & TradeFi Ltd. In Conjunction With The Nigerian Youth Parliament." />
+        <meta name="twitter:image" content="{{asset('images/nyp-logo.png')}}" />
+        <meta name="twitter:card" content="summary" />
 
         <!-- Open Graph Meta Tags -->
-        <meta property="og:title" content="NYP-IP Portal" />
-        <meta property="og:description" content="NYP-IP Is A National-Scale, Youth-Driven Industrialization, Entrepreneurship and DeFi Platform for Innovation, Inclusion & Growth. NYP-IP Is Enabling 10 Million Nigerian Youths To Become Builders Of Real Industry. This Initiative Is Powered by LuxuryX Technologies & TradeFi Ltd. In Conjunction With The Nigerian Youth Parliament." />
-        <meta property="og:image" content="{{asset('images/logo.svg')}}" />
+        <meta property="og:title" content="NYP-IP Portal | Industrialization" />
+        <meta property="og:description" content="NYP-IP Is A National-Scale, Youth-Driven Industrialization & Entrepreneurship Platform Enabling Nigerian Youths To Become Builders Of Real Industry. This Initiative Is Powered by LuxuryX Technologies & TradeFi Ltd. In Conjunction With The Nigerian Youth Parliament." />
+        <meta property="og:image" content="{{asset('images/nyp-logo.png')}}" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://nypipportal.luxuryxtech.org.ng" />
         <meta property="og:image:alt" content="NYP-IP Portal" />
+        <meta property="og:image:width" content="400">
+        <meta property="og:image:height" content="400">
 
         <!-- Scripts -->
         @routes
